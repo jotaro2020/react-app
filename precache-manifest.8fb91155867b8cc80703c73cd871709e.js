91ee2639567215cee7672b58c5416c0743c56b6d
Zhassulan Abishev <zhaslan27abishev@gmail.com>
2020-09-12 19:50:22 +0600

    Updates

---
+++ precache-manifest.8fb91155867b8cc80703c73cd871709e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd97913914f7c84412948b47404f44f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b1bb411f2a4070aa7048",
+    "url": "/react-app/static/css/main.7da9cc54.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bb411f2a4070aa7048",
+    "url": "/react-app/static/js/main.616cb879.chunk.js"
+  },
+  {
+    "revision": "ecdd09c39530c95244f3",
+    "url": "/react-app/static/js/runtime-main.00f461f2.js"
+  }
+]);
